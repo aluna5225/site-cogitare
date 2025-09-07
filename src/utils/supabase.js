c99b53571f1e0a2fc992786ec7dccdf10134fcc2
Ana Luísa Scarpa de Oliveira <analu.scarpa.oliveira@gmail.com>
2025-09-07 14:01:23 -0300

    first commit

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,8 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ajmdhxsoqqqjhbromitp.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFqbWRoeHNvcXFxamhicm9taXRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcyMDM5MTQsImV4cCI6MjA3Mjc3OTkxNH0.wkEtaKq6vRI82O68o60FULkJMUSmJX_e07IVauZlmso';
+const supabaseUrl = 'https://cmvsglbgaqangzaxgesp.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtdnNnbGJnYXFhbmd6YXhnZXNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzODk5NTgsImV4cCI6MjA2NDk2NTk1OH0.x5XXguwM_-1VLsJ_vw9A8NcsqWOXblMspTFZcOuAF2c';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
