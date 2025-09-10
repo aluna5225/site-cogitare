736dd743863fac912c1827044d092d90c62fb60b
Ana Luísa Scarpa de Oliveira <analu.scarpa.oliveira@gmail.com>
2025-09-10 19:55:57 -0300

    first commit

--- src/pages/aplicativo.jsx
+++ src/pages/aplicativo.jsx
@@ -13,14 +13,22 @@ function Aplicativo() {
 
     return (
         <>
-            <main>
-                <h1>Olá! Você está logado!</h1>
-                <h2>baixe agora nosso app e comece seu apredizado conosco.</h2>
-                <a href={app}>Click para fazer download do App</a>
-                <br /><br /><br />
+            <main className="container-app">
+                
+                <h1>Olá, seja bem vindo a COGITARE</h1>
+                
+                <h2>Agora você tem acesso à sua conta da cogitare, e pode baixar o aplicativo aqui mesmo, pelo nosso site!</h2>
+              
+                <h2>Basta você clicar no link abaixo, e você será redirecionado para o local de upload do app.</h2>
+                <a href={app}>Click para fazer download do App </a>
+                <br /><br />
+                
+                <h4>caso queira fazer o logout, é so clicar no botâo a baixo</h4>
                 <button onClick={signOut}>LogOut</button>
             </main>
+            
             <br /><br /><br /><br /><br />
+            
             <footer>
                 &copy; 2025 Cogitare - Todos os direitos reservados para a empresa DigitalInovaTecnologia.
             </footer>                
